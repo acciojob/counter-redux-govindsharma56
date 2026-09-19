06802e5d062c21be67fca7ed72237b2eb06f7ffb
unknown <govindsharmags1057@gmail.com>
2026-09-19 19:22:28 +0530

    update app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
 
 import React from "react";
 import './../styles/App.css';
-
+import { increment,decrement } from "./counterSlice";
+import { useDispatch,useSelector } from "react-redux";
 const App = () => {
+      const dispatch = useDispatch();
+      const count = useSelector((state) => state.counter.value);
   return (
     <div>
-        {/* Do not remove the main div */}
+        <p>Count  {count}</p>
+        <button onClick={()=>dispatch(increment())}>Increment</button>
+        <button onClick={()=>dispatch(decrement())}>Decrement</button>
     </div>
   )
 }
